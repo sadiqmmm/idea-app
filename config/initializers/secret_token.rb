9ce9d28ec2985bd7be07a2b76a026bf27ac41ada
mohammed sadiq <sadiqmmm@gmail.com>
2013-04-26 15:46:40 +0530

    Idea note Taking app (added bootstrap + comments + image uploads feature )

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsboys::Application.config.secret_token = '229bd1c97233796c0fa3e765f2d98ce07a7664d9383784103b73c68dfbc925fdf0d3cc1197d4478f8840fcf76b1de166b8339a4b698aa0f4a3562f4e2a376daa'
